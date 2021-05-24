221d3434d555e1307cee56458ea267d870a431d1
vanderL <vnd.vander@gmail.com>
2021-05-23 22:34:50 -0300

    Login Styles with backgroud

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -5,21 +5,26 @@ import LoginCreate from './LoginCreate';
 import LoginForm from './LoginForm';
 import LoginPasswordLost from './LoginPasswordLost';
 import LoginPasswordReset from './LoginPasswordReset';
+import styles from './Login.module.css';
 
 const Login = () => {
     const {login} = React.useContext(UserContext);
 
     if(login === true) return <Navigate to="/conta" />
 
-    return <div>
-        <Routes>
-            <Route path="/" exact element={<LoginForm />}/>
-            <Route path="criar" element={<LoginCreate />}/>
-            <Route path="perdeu" element={<LoginPasswordLost />}/>
-            <Route path="reset" element={<LoginPasswordReset />}/>
+    return (
+        <section className={styles.login}>
+            <div className={styles.forms}>
+                <Routes>
+                    <Route path="/" exact element={<LoginForm />}/>
+                    <Route path="criar" element={<LoginCreate />}/>
+                    <Route path="perdeu" element={<LoginPasswordLost />}/>
+                    <Route path="reset" element={<LoginPasswordReset />}/>
 
-        </Routes>
-    </div>
+                </Routes>
+            </div>
+        </section>
+    ) 
 }
 
 export default Login;
