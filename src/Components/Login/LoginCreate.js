3d0e8fb8dfb72beb12bee761fa0db0386098ce4d
vanderL <vnd.vander@gmail.com>
2021-06-01 22:49:00 -0300

    Create account and after automatic login

--- src/Components/Login/LoginCreate.js
+++ src/Components/Login/LoginCreate.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
+import useForm from '../../Hooks/useForm';
+import { USER_POST } from '../../Service/api';
+import {UserContext} from '../../Context/UserContext';
 
 const LoginCreate = () => {
+    const username = useForm();
+    const email = useForm('email');
+    const password = useForm();
+
+    const {userLogin} = useContext(UserContext);
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        const {url, options} = USER_POST({
+            username: username.value,
+            email: email.value,
+            password: password.value,
+        })
+        const response = await fetch(url, options);
+        if (response.ok) userLogin(username.value, password.value);
+
+
+    }
+    
     return (
-        <div>
-            Login Create
-        </div>
+        <section classname="animeLeft">
+            <h1 className="title">Cadastre-se</h1>
+            <form onSubmit={handleSubmit}>
+                <Input label="Usuário" type="text" name="username" {...username}/>
+                <Input label="Email" type="email" name="email" {...email}/>
+                <Input label="Usuário" type="password" name="password" {...password}/>
+                <Button>Cadastrar</Button>
+            </form>
+        </section>
     )
 }
 
