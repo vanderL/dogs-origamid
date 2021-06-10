c26eb60a18be7535c3bfad5728ec31f88a1b29a3
vanderL <vnd.vander@gmail.com>
2021-06-10 11:17:42 -0300

    Photo Post

--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -5,6 +5,10 @@ const types = {
         regex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
         message: 'Please enter a valid email address',
     },
+    number: {
+        regex: /^\d+$/,
+        message: 'Please enter a valid number',
+    }
 };
 
 const useForm = (type) => {
