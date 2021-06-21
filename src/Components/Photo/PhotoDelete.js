d131a9931c4dbb5ba50a7db4d6e4a0c545d32ca6
vanderL <vnd.vander@gmail.com>
2021-06-21 15:08:28 -0300

    Component PhotoDelete

---
+++ src/Components/Photo/PhotoDelete.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import styles from './PhotoDelete.module.css';
+import { PHOTO_DELETE } from '../../Service/api';
+import useFetch from '../../Hooks/useFetch';
+
+const PhotoDelete = ({ id }) => {
+    const { loading, request } = useFetch();
+
+    async function handleClick(e) {
+        const confirm = window.confirm('Tem certeza ?');
+
+        if(confirm) {
+            const { url, options } = PHOTO_DELETE(id);
+            const { response } = await request(url, options);
+            if (response.ok) window.location.reload();
+        }
+
+    }
+
+    return (
+        <>
+            {loading ? (
+                <button disabled className={styles.delete}>
+                    Deletando
+                </button>
+            ) : (
+                <button onClick={handleClick} className={styles.delete}>
+                    Deletar
+                </button>
+            )} 
+        </>
+    )
+}
+
+export default PhotoDelete;
