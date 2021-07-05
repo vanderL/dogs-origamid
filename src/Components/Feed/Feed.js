53e84ca8a029ce13f4e984044a10923136ffa74c
vanderL <vnd.vander@gmail.com>
2021-07-05 16:02:23 -0300

    My Photos

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -2,13 +2,13 @@ import React from 'react';
 import FeedModal from './FeedModal';
 import FeedPhotos from './FeedPhotos';
 
-const Feed = () => {
+const Feed = ({user}) => {
     const [modalPhoto, setMoldaPhoto] = React.useState(null);
 
     return (
         <div>
             {modalPhoto && <FeedModal photo={modalPhoto} setMoldaPhoto={setMoldaPhoto}/>}
-            <FeedPhotos setMoldaPhoto={setMoldaPhoto} />
+            <FeedPhotos user={user} setMoldaPhoto={setMoldaPhoto} />
         </div>
     )
 }
--- src/Components/Feed/FeedPhotos.js
+++ src/Components/Feed/FeedPhotos.js
@@ -9,19 +9,19 @@ import Loading from '../../Helper/Loading';
 
 import styles from './FeedPhotos.module.css';
 
-const FeedPhotos = ({setMoldaPhoto}) => {
+const FeedPhotos = ({user, setMoldaPhoto}) => {
     const {data, loading, error, request} = useFetch();
 
     React.useEffect(() => {
         async function fetchPhotos(){
-            const {url, options} = PHOTOS_GET({ page: 1, total: 6, user: 0});
+            const {url, options} = PHOTOS_GET({ page: 1, total: 4, user});
             const {json} = await request(url, options);
 
             console.log(json);
         }
 
         fetchPhotos();
-    }, [request])
+    }, [request, user])
 
     if (error) return <Error error={error} />
     if (loading) return <Loading />
