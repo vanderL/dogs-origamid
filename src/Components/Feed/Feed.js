d52beddabe1513ebfb0fd29109e4d3ad88ef6fd2
vanderL <vnd.vander@gmail.com>
2021-07-05 18:02:55 -0300

    Scroll Infinite

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -1,14 +1,52 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import FeedModal from './FeedModal';
 import FeedPhotos from './FeedPhotos';
 
 const Feed = ({user}) => {
-    const [modalPhoto, setMoldaPhoto] = React.useState(null);
+    const [modalPhoto, setMoldaPhoto] = useState(null);
+    const [pages, setPages] = useState([1]);
+    const [infinite, setInfinite] = useState(true);
+
+    useEffect(() => {
+        let wait = false;
+
+        function infiniteScroll(){
+            if(infinite){
+                const scroll = window.scrollY;
+                const height = document.body.scrollHeight - window.innerHeight;
+                if (scroll > height * 0.75 && !wait) {
+                    setPages((pages) => [...pages, pages.length + 1]);
+                    wait = true;
+    
+                    setTimeout(() => {
+                        wait = false;
+                    }, 3000);
+                }
+            }
+        }
+
+        window.addEventListener('wheel', infiniteScroll);
+        window.addEventListener('scroll', infiniteScroll);
+        return () => {
+            window.addEventListener('wheel', infiniteScroll);
+            window.addEventListener('scroll', infiniteScroll);
+        };
+
+    }, [infinite]);
 
     return (
         <div>
             {modalPhoto && <FeedModal photo={modalPhoto} setMoldaPhoto={setMoldaPhoto}/>}
-            <FeedPhotos user={user} setMoldaPhoto={setMoldaPhoto} />
+            {pages.map(page => 
+                <FeedPhotos 
+                    user={user} 
+                    key={page} 
+                    page={page} 
+                    setMoldaPhoto={setMoldaPhoto}
+                    setInfinite={setInfinite}
+                />
+            )}
+            
         </div>
     )
 }
--- src/Components/Feed/FeedPhotos.js
+++ src/Components/Feed/FeedPhotos.js
@@ -9,19 +9,21 @@ import Loading from '../../Helper/Loading';
 
 import styles from './FeedPhotos.module.css';
 
-const FeedPhotos = ({user, setMoldaPhoto}) => {
+const FeedPhotos = ({page, user, setMoldaPhoto, setInfinite}) => {
     const {data, loading, error, request} = useFetch();
 
     React.useEffect(() => {
         async function fetchPhotos(){
-            const {url, options} = PHOTOS_GET({ page: 1, total: 4, user});
-            const {json} = await request(url, options);
+            const total = 3;
+            const {url, options} = PHOTOS_GET({ page, total, user});
+            const {response, json} = await request(url, options);
+            if( response && response.ok && json.length < total ) setInfinite(false);
 
             console.log(json);
         }
 
         fetchPhotos();
-    }, [request, user])
+    }, [request, user, page, setInfinite])
 
     if (error) return <Error error={error} />
     if (loading) return <Loading />
