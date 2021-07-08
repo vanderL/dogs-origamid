066a4f47cb8f9282dcf14949c7f65394e1bb0e87
vanderL <vnd.vander@gmail.com>
2021-07-08 16:02:23 -0300

    Footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './Footer.module.css';
+import {ReactComponent as Dogs} from '../Assets/dogs-footer.svg';
 
 const Footer = () => {
     return (
-        <div className={styles.Footer}>
-            Footer
-        </div>
+        <footer className={styles.footer}>
+            <Dogs />
+            <p>Dogs. Alguns direitos reservados.</p>
+        </footer>
     )
 };
 
