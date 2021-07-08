6945b22214ba6ac1715a8a0361c7d36b26f6827b
vanderL <vnd.vander@gmail.com>
2021-07-08 18:42:32 -0300

    Add React.lazy e React.Suspense

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -1,10 +1,10 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, lazy, Suspense} from 'react';
 import Head from '../../Helper/Head';
 import useFetch from '../../Hooks/useFetch';
 import { STATS_GET } from '../../Service/api';
 import Loading from '../../Helper/Loading';
 import Error from '../../Helper/Error';
-import UserStatsGraphs from './UserStatsGraphs';
+const UserStatsGraphs = lazy(() => import('./UserStatsGraphs'))
 
 const UserStats = () => {
     const {data, error, loading, request} = useFetch();
@@ -22,10 +22,10 @@ const UserStats = () => {
     if(error) return <Error error={error} />;
     if(data)
         return (
-            <div>
+            <Suspense fallback={<div></div>}>
                 <Head title="Estatísticas" />
                 <UserStatsGraphs data={data} />
-            </div>
+            </Suspense>
         );
     else return null;
     
